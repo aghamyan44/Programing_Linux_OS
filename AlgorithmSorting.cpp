3dac8a78aab88668a176abdaf41f280b77efbb7f
aghamyan44 <juliaaghamyan44@gmail.com>
2021-02-20 22:19:26 +0400

    Homework_4_new

--- AlgorithmSorting.cpp
+++ AlgorithmSorting.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath> 
 
-void print(const std::vector<int>& v) 
+void print(const std::vector<int>& v)
 {
-	for (auto& i : v) 
+	for (auto& i : v)
 	{
-		std::cout << i  << "  ";
+		std::cout << i << "  ";
 	}
 }
 
 int main()
 {
-	std::vector<int> v = { 5, 7, 4, 2, 8, 6, 1, 9, 0, 3 };
+	std::vector<int> v = { 5, 7, 4, -2, 8, -6, 1, -9, 0, 3 };
 	std::cout << "Vector Not Sorting: ";
 	print(v);
 
-	std::sort(v.begin(), v.end(), [](int a, int b) { return a > b; });
-	
+	std::sort(v.begin(), v.end(), [](int a, int b) { return abs(a) > abs(b); });
+
 	std::cout << std::endl << "Vector Sorting: ";
 	print(v);
 }
